09f249689cf48eae9a2c41a4e755a0c3d7184bfe
Dark Shadow <dark256.256.256.256shadow@gmail.com>
2025-08-08 19:58:03 +0530

    Delete backend directory

--- backend/routes/dashboardRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { protect, teacher } from '../middleware/authMiddleware.js';
-import { getTeacherDashboardStats } from '../controllers/dashboardController.js';
-
-const router = express.Router();
-
-router.get('/teacher', protect, teacher, getTeacherDashboardStats);
-
-export default router;
